31534727a1c08ce5be52ef78b658ec0954c1fdaa
bpkadpalu20-star <bpkadpalu20@gmail.com>
2025-08-09 21:15:52 +0800

    first commit

--- app/Models/RincianObjekPenerimaan.php
+++ app/Models/RincianObjekPenerimaan.php
@@ -9,7 +9,7 @@ class RincianObjekPenerimaan extends Model
 {
     use HasFactory;
 
-    public $table = "rincianobjekPenerimaan";
+    public $table = "rincianobjekpenerimaan";
 
     protected $fillable = [
         'id','id_op', 'id_op', 'id_opd', 'id_akun', 'id_kelompok', 'kd_jenis', 'kd_objek', 'kd_rincianobjek', 'kode_rincianobjek', 'nilai_pagurincian', 'nilai_realisasirincian', 'bulan_id', 'bulan'
